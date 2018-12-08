45a96567702e975da0166c774ec3f3db415ae8cc
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-08 01:22:09 -0300

    ++table and semantic

--- src/controllers/SemanticAnalyser.java
+++ src/controllers/SemanticAnalyser.java
@@ -1,12 +1,25 @@
 package controllers;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import models.value.Token;
+import util.TableOfSymbols;
+import util.TokenType;
+
 public class SemanticAnalyser {
     private static SemanticAnalyser instance;
+    private LinkedList<TableOfSymbols> listTables = new LinkedList<>();
     
     public static SemanticAnalyser getInstance(){
         if (instance == null) {
             instance = new SemanticAnalyser();
         }
         return instance;
+    }
+    
+    public void CreateTableSymb(Queue<Token> queue){
+        if (queue != null && !queue.isEmpty()) {
+        }
     }
 }
--- src/util/TableOfSymbols.java
+++ src/util/TableOfSymbols.java
@@ -1,12 +1,63 @@
 package util;
 
+import models.value.Token;
+
 public class TableOfSymbols {
     private static TableOfSymbols instance;
+    //If necessary remove or add more
+    private Token token;
+    private String categoria;
+    private String tipo;
+
+    public TableOfSymbols(Token token, String categoria, String tipo) {
+        this.token = token;
+        this.categoria = categoria;
+        this.tipo = tipo;
+    }
+
+    public TableOfSymbols() {
+    }
     
     public static TableOfSymbols getInstace(){
         if (instance == null) {
             instance = new TableOfSymbols();
         }
         return instance;
+    }
+
+    public static TableOfSymbols getInstance() {
+        return instance;
+    }
+
+    public static void setInstance(TableOfSymbols instance) {
+        TableOfSymbols.instance = instance;
+    }
+
+    public Token getToken() {
+        return token;
+    }
+
+    public void setToken(Token token) {
+        this.token = token;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    public String toString(){
+        return "Categoria -> " + this.categoria + " -> " + this.token.toString();
     }
 }
